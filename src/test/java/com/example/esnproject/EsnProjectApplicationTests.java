bf9c2e75422632348a81a786d576eae07411fb35
olneha <olneha@ttu.ee>
2024-03-19 21:34:32 +0200

    setting-project: add Liquibase, sample db schema, entity, repository and controller

--- src/test/java/com/example/esnproject/EsnProjectApplicationTests.java
+++ src/test/java/com/example/esnproject/EsnProjectApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.esnproject;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EsnProjectApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.example.esnproject;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EsnProjectApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
