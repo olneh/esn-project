809b140843ce2383dee7fa0e8d9fd16c919a7c97
olneha <olneha@ttu.ee>
2024-03-31 12:22:44 +0300

    setting-project: final keyword add

--- src/main/java/com/example/esnproject/controllers/RegistrationController.java
+++ src/main/java/com/example/esnproject/controllers/RegistrationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RegistrationController {
 
-    private MemberRepository dao;
+    private final MemberRepository dao;
 
     public RegistrationController(MemberRepository dao) {
         this.dao = dao;
