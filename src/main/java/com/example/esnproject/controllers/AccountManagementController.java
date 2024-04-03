a1d0972d798c7a77d6ba4841b62ea1b18c124ca5
olneha <olneha@ttu.ee>
2024-04-03 13:06:49 +0300

    setting-project: add account management controller

---
+++ src/main/java/com/example/esnproject/controllers/AccountManagementController.java
@@ -0,0 +1,40 @@
+package com.example.esnproject.controllers;
+
+import com.example.esnproject.entities.Member;
+import com.example.esnproject.repositories.MemberRepository;
+import com.example.esnproject.services.MemberService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/account")
+public class AccountManagementController {
+
+//    private final MemberRepository memberRepository;
+    private final MemberService memberService;
+
+    public AccountManagementController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/register")
+    public Member saveMember(@RequestBody Member member){
+        return memberService.createMember(member);
+    }
+
+    @GetMapping("/allMembers")
+    public List<Member> getMembers(){
+        return memberService.getMembers();
+    }
+
+//    @PostMapping("/login")
+//    public Member loginMember(@RequestBody Member member){
+//        return memberRepository.save(member);
+//    }
+//
+//    @GetMapping("/allMembers")
+//    public List<Member> getMembers(@RequestBody Member member){
+//        return memberRepository.findAll();
+//    }
+}
