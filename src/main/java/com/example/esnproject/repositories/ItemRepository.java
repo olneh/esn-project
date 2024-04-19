e99c83bf2034c5a7f48f565eaba59d643f0af17e
olneha <olneha@ttu.ee>
2024-04-19 17:33:13 +0300

    1 setting-project: db add sample data, remove sample Item repository, controller, class

--- src/main/java/com/example/esnproject/repositories/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.esnproject.repositories;
-
-import com.example.esnproject.entities.Item;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends CrudRepository<Item, Long> {
-}
