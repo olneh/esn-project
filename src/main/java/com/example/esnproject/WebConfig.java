d26dfb78bb3d4842662ff1d1c98cd1ec4f99054d
olneha <olneha@ttu.ee>
2024-04-19 17:28:02 +0300

    2 security: cors mapping

--- src/main/java/com/example/esnproject/WebConfig.java
+++ src/main/java/com/example/esnproject/WebConfig.java
@@ -8,9 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("https://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
+
 }
